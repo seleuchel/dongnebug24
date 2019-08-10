74ea60c9e79d1a613197da913f71b50b42a3e938
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-10 15:25:33 +0900

    Update settings.py
    Edit with ALLOWED_HOSTS, TIME_ZONE, USE_TZ

--- dongnebug24/settings.py
+++ dongnebug24/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '$fhivp2m)zam^n3&t)ckg&o^$-9e&m$0qp@bgsrw)tovjpt&)='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
