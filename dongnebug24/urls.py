0b3adfcb5522009028e804e1578b8bd6b1e17997
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-11 15:13:28 +0900

    Debuged url dismatch problem
    Edited right module and right path

--- dongnebug24/urls.py
+++ dongnebug24/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from dongnebug import views
+from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -24,6 +24,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
