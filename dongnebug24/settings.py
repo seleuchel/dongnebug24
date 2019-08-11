10ae4c74368c905244a15af4423f3a77d0152717
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-11 15:03:24 +0900

    Update settings to apply rest_framework
    Added Installed Apps, REST_FRAMEWORK pagination settings

--- dongnebug24/settings.py
+++ dongnebug24/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'dongnebug',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
