19f1e6969e00f83b3ea758b8f9f4840a30fa0302
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-15 00:16:49 +0900

    Tested views.py for redirection when create is complete
    tried several cases  to fix it.. but it need to fix againt.

--- dongnebug/views.py
+++ dongnebug/views.py
@@ -3,7 +3,6 @@ from django.views.generic import (
     CreateView, ListView, DetailView, DeleteView,
     TemplateView,UpdateView, FormView)
 from django.contrib.auth.models import User
-# from django.contrib.auth.views import LoginView
 from django.urls import (reverse, reverse_lazy)
 from django.utils import timezone
 from datetime import timedelta
@@ -11,25 +10,25 @@ from . import models
 from . import forms
 import random
 
-# class LoginView(FormView):
-#     template_name = 'login.html'
-#     form_class = forms.AccountForm
-#     model = User
-#
-#     def form_valid(self, form):
-#         form.save()
-#         return super(LoginView, self).form_valid(form)
-#
-#     def get_absolute_url(self):
-#         return reverse('login')
-
-class IndexView(TemplateView):
-    template_name = 'login.html'
-
-
 class RegisterView(CreateView):
     form_class = forms.RegisterForm
     template_name = 'login.html'
     success_url = reverse_lazy('login')
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class CreateComplainView(CreateView):
+    form_class = forms.ComplainForm
+    template_name = 'new_complain.html'
+
+
+    def get_absolute_url(self):
+        return reverse('dongnebug:index')
+
+
+
 
 
