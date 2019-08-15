3e9c05896bcdc7f6347de7988d92b9d2db8770e4
jso4967 <jso4967@gmail.com>
2019-08-15 16:05:09 +0900

    Applied OnetoOneField for proper test
    Add user_id with OnetoOneField(primary_key=True)..

--- api/models.py
+++ api/models.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import User
 
 class Location(models.Model):
 
-    # user = models.OneToOneField(
-    #     User,
-    #     on_delete=models.CASCADE,
-    #     primary_key=True,
-    # )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 
     latitude = models.DecimalField(max_digits=20, decimal_places=17, null=True)
     longitude = models.DecimalField(max_digits=20, decimal_places=17, null=True)
