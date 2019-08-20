e196e3f903dfcbefaa6b8c16b844bea2821b973d
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-20 15:52:52 +0900

    Update ComplainForm, Create ProfileForm

--- dongnebug/forms.py
+++ dongnebug/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Complain, Favorite, Comment, Sympathy
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -16,22 +17,58 @@ class SearchForm(forms.ModelForm):
 
 
 class ComplainForm(forms.ModelForm):
-
     class Meta:
         model = Complain
         fields = ('title', 'content', 'file', 'latitude', 'longitude')
+        labels = {
+            'title': '',
+            'content': '',
+            'latitude': '',
+            'longitude': ''
+        }
         widgets = {
             'title': forms.TextInput(
                 attrs={
-                    'id': 'text'
+                    'size': '20',
+                    'class': 'form-control',
+                    'placeholder': "위치를 찾아보세요."
                 }
+
             ),
             'content': forms.TextInput(
                 attrs={
-                    'id': 'text'
+                    'class': 'form-control',
+                    'placeholder': "내용을 입력하세요",
+                    'aria-label': "With textarea"
+                }
+            ),
+            'latitude': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': "위도"
+                }
+            ),
+            'longitude': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': "경도"
                 }
             ),
         }
 
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username',)
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': "컴공의 누군가",
+                    'style': "font-family:'Nanum Gothic', sans-serif"
+                }
+            )
+        }
 
+
+class
