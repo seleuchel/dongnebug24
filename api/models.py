c8b6ef9d4a7faa82233c81adee7d9d1097f0f1f5
jso4967 <33389418+jso4967@users.noreply.github.com>
2019-08-20 23:29:30 +0900

    Update Locations model
    author relationship : OnetoOneField

--- api/models.py
+++ api/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Locations(models.Model):
 
-    author = models.ForeignKey(
+    author = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
     )
-    token = models.CharField(max_length=100, null = True)
+    token = models.CharField(max_length=100, null=True)
     latitude = models.DecimalField(max_digits=20, decimal_places=17, null=True, default=0)
     longitude = models.DecimalField(max_digits=20, decimal_places=17, null=True, default=0)
 
