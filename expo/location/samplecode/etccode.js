08ed77e84ba800b724f9048b4c49dadb7e76a32e
seleuchel <smsky1010@gmail.com>
2019-08-20 17:00:40 +0900

    ok

---
+++ expo/location/samplecode/etccode.js
@@ -0,0 +1,10 @@
+// //귀주 : readLocation
+// ReadLocation(){
+//     return fetch('http://168.131.153.40:8000/location/7/', {
+//     method: 'GET'
+//     })
+//     .then((response) => response.json())
+//     .catch((error) => {
+//        console.error(error);
+//     });
+// }
